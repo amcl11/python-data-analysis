feb8d26d614a05026050b7fdd43e22470e1fb136
amcl11 <132857824+amcl11@users.noreply.github.com>
2023-06-20 12:56:11 +1000

    updated both scripts

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,9 +2,10 @@
 import os
 import csv
 
-#Path to collect data from the Resources folder
+#Establish path to collect the CSV file from the Resources folder
 budget_data_csv = os.path.join('Resources', 'budget_data.csv')
 
+#open the CSV file
 with open(budget_data_csv) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     
@@ -23,12 +24,13 @@ with open(budget_data_csv) as csvfile:
     greatest_decrease = 0
     greatest_decrease_month = ""
     
-    #loop through each row to add up total amount of months and calculate Net Total for P/L column
+    #loop through each row to add up total amount of months 
     for row in csvreader:
         month_count += 1
         monthly_amount = int(row[1])
         net_total += monthly_amount        
       
+        # calculate Net Total for P/L column
         profit = int(row[1])
         if previous_profit != 0:
             change = profit - previous_profit
@@ -40,34 +42,34 @@ with open(budget_data_csv) as csvfile:
                 greatest_increase = change
                 greatest_increase_month = row[0]
             
-            
+            #calculating Greatest Decrease
             if change < greatest_decrease:
                 greatest_decrease = change
                 greatest_decrease_month = row[0]
                 
-        previous_profit =  profit
+        previous_profit = profit
         
+    #calculate average P/L change over the period     
     average_change = total_change / change_count if change_count > 0 else 0
     month_total = month_count
     
-    print('Financial Analysis');
-    print('-----------------------------');
-    print(f'Total Months: {month_total}');
-    print(f'Total: ${net_total}');
-    print(f"Average Change: ${average_change:.2f}");
-    print(f'Greatest Increase in Profits: {greatest_increase_month}: (${greatest_increase})')    
-    print(f'Greatest Decrease in Profits: {greatest_decrease_month}: (${greatest_decrease})')    
+    #print found values using spaces and lines per the example in BootCampSpot making it easier to read. 
+    print('\nFinancial Analysis');
+    print('\n-----------------------------');
+    print(f'\nTotal Months: {month_total}');
+    print(f'\nTotal: ${net_total}');
+    print(f'\nAverage Change: ${average_change:.2f}');
+    print(f'\nGreatest Increase in Profits: {greatest_increase_month}: (${greatest_increase})')    
+    print(f'\nGreatest Decrease in Profits: {greatest_decrease_month}: (${greatest_decrease})\n')    
       
-  
-  
-    #Specify location / folder for new analysis txt file
+    #Specify location / folder for new analysis txt file to be written to 
     outpath = os.path.join('analysis', 'PyBank_Analysis.txt')
 
     with open(outpath, 'w') as f:
-        print('Financial Analysis', file=f)
-        print('-----------------------------', file=f)
-        print(f'Total Months: {month_total}', file=f)
-        print(f'Total: ${net_total}', file=f)
-        print(f'Average Change: ${average_change:.2f}', file=f)
-        print(f'Greatest Increase in Profits: {greatest_increase_month}: (${greatest_increase})', file=f)    
-        print(f'Greatest Decrease in Profits: {greatest_decrease_month}: (${greatest_decrease})', file=f)   
+        print('\nFinancial Analysis', file=f)
+        print('\n-----------------------------', file=f)
+        print(f'\nTotal Months: {month_total}', file=f)
+        print(f'\nTotal: ${net_total}', file=f)
+        print(f'\nAverage Change: ${average_change:.2f}', file=f)
+        print(f'\nGreatest Increase in Profits: {greatest_increase_month}: (${greatest_increase})', file=f)    
+        print(f'\nGreatest Decrease in Profits: {greatest_decrease_month}: (${greatest_decrease})\n', file=f)   
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,58 +1,75 @@
-#import modules
 import os
 import csv
 
-#Path to collect data from the Resources folder
+# Path to collect data from the Resources folder
 election_data_csv = os.path.join('Resources', 'election_data.csv')
 
 with open(election_data_csv) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
-    
-    #skip / store Header row 
+
+    # skip / store Header row
     csv_header = next(csvreader)
-    
+
     # set vote count variable to 0
     vote_count = 0
-    
-    #initialise list for list of candidates
+
+    # initialise list for list of candidates
     candidates = []
-    
-    #Create Dictionary to store vote counts for each candidates
+
+    # Create Dictionary to store vote counts for each candidate
     candidate_votes = {}
-    
-    #start loop to count total number of votes, populate names into the candidates list, and sum candidate votes 
+
+    # start loop to count total number of votes, populate names into the candidates list, and sum candidate votes
     for row in csvreader:
         vote_count += 1
-        
-        name = row[2] #add names to candidate list
-        
+
+        name = row[2]  # add names to candidate list
+
         if name not in candidates:
-            candidates.append(name) 
-            candidate_votes[name] = 0 #initialise vote count for each candidate to zero
-            
-        candidate_votes[name] += 1 #Increment vote count by 1 for the corresponding candidate 
-        
-    print('\nElection Results \n\n----------------------------')
-    print(f'\nTotal Votes: {vote_count}\n\n----------------------------')
-    
-    #initialise loop to calculate percent of votes for each candidate 
+            candidates.append(name)
+            candidate_votes[name] = 0  # initialise vote count for each candidate to zero
+
+        candidate_votes[name] += 1  # Increment vote count by 1 for the corresponding candidate
+
+    # Define separator and line space strings
+    separator = '-' * 30
+    line_space = '\n'
+
+    # Create the output string using f-strings and proper formatting
+    output = f"""
+Election Results \n
+{separator} \n
+Total Votes: {vote_count} \n
+{separator}\n""" 
+
+    # initialise loop to calculate percent of votes for each candidate
     for candidate, votes in candidate_votes.items():
         percent_of_votes = votes / vote_count * 100
-        print(f'\n{candidate}: {percent_of_votes:.3f}% ({votes})')
-       
-    #set variables for Max Votes and Winner
+
+        output += f"""
+{candidate}: {percent_of_votes:.3f}% ({votes})\n""" 
+
+    # set variables for Max Votes and Winner
     max_votes = 0
     winner = ""
 
-    #initialise loop to find the candidate with the highest vote count
+    # initialise loop to find the candidate with the highest vote count
     for candidate, votes in candidate_votes.items():
         if votes > max_votes:
             max_votes = votes
             winner = candidate
-    print(f'\n---------------------------- \n\nWinner: {winner}\n\n----------------------------')
-   
-    
-        #Specify location / folder for new analysis txt file
+
+    output += f"""
+{separator}\n
+Winner: {winner}\n
+{separator}\n"""
+
+    # Specify location / folder for new analysis txt file
     outpath = os.path.join('analysis', 'PyPoll_Analysis.txt')
 
-    
+    # Write the output to the file
+    with open(outpath, 'w') as f:
+        f.write(output)
+
+    # Print the output to the console
+    print(output)
